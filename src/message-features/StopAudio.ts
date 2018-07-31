68e61a04e7f9cb58f78f3e41053af8f36c5067c4
Hayden Hong <afruitpie@gmail.com>
2018-07-31 10:26:26 -0700

    Began feature to remove "stop" messages

--- src/message-features/StopAudio.ts
+++ src/message-features/StopAudio.ts
@@ -7,6 +7,13 @@ export class StopAudio extends BaseHandler {
     if (this.util.messageContains(Commands.Stop) || this.util.commandIs(Commands.Stop)) {
       console.log("Stopping audio");
       this.util.stopAudio();
+      const messageText = this.util.getMessageText();
+
+      // TODO: This makes it impossible for someone to just say "stop"
+      // Deletes message if all it does is stop audio
+      // if (messageText === Commands.Stop || messageText === `;;${Commands.Stop})`) {
+      //   this.util.getMessage().delete();
+      // }
     }
   }
 }
